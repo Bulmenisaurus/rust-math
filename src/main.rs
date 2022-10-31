9037e6e0a07d9f6f0c436326960a285540c35858
a <alexeybulmeni@gmail.com>
2022-10-30 21:04:10 -0700

    use const and arrays instead of vectors

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::cmp;
 
+const DIGITS_LEN: usize = 25;
+
 fn main() {
-    let mut digits: Vec<u8> = vec![1];
+    let mut digits = [-1; DIGITS_LEN];
+    digits[0] = 1;
     let mut i: u64 = 0;
-    let digits_len: usize = 20;
-    println!("checking last {} digits", digits_len);
+    println!("checking last {} digits", DIGITS_LEN);
 
     loop {
-        if is_only_pow2(digits.clone()) {
+        if is_only_pow2(digits) {
             println!("found 2^{}", i);
             // not 128
             if i > 7 {
@@ -19,43 +21,52 @@ fn main() {
             println!("Scanned {}", i);
         }
         
-        digits = mul_2_digits(digits.clone(), digits_len);
+        digits = mul_2_digits(digits);
         i += 1;
     }
 }
 
 // digits are stored back to front, least significant in the front
-fn mul_2_digits(digits: Vec<u8>, max_length: usize) -> Vec<u8> {
-    let mut new_digits: Vec<u8> = vec![0; cmp::min(digits.len(), max_length)];
+fn mul_2_digits(digits: [i8; DIGITS_LEN]) -> [i8; DIGITS_LEN] {
+    let mut new_digits: [i8; DIGITS_LEN] = [-1; DIGITS_LEN];
+
+    // manually set the first digit to 0, as loop sets next digit with carry calculations
+    new_digits[0] = 0;
 
     
-    digits.iter().enumerate().for_each(|(i, item)| {
-        if i >= max_length {
+    digits.iter().enumerate().for_each(|(i, &item)| {
+        // if the digit is unset, stop (0 * 2 = 0)
+        if item == -1 {
             return;
         }
 
+        // guaranteed to fit in, as item <= 9 and 2*item <= 18 < 127
         let new_digit_value = item * 2;
         let digit = new_digit_value % 10;
         let carry = (new_digit_value - digit) / 10;
 
-        new_digits[i] += digit;
+        if new_digits[i] == -1 {
+            new_digits[i] = digit;
+        } else {
+            new_digits[i] += digit;
+        }
 
-        // clamp to 20
-        if carry == 0 || i + 1 == max_length {
+
+        let next_digit = carry;
+        // if currently at the DIGITS_LENth digit, don't set next (as it is out of bounds)
+        if i + 1 == DIGITS_LEN {
             return;
+        } else if carry != 0 {
+            // otherwise, set the next digit to the carry otherwise
+            new_digits[i + 1] = next_digit;
         }
-
-        if new_digits.len() < i+2{
-            new_digits.push(0);
-        }
-        new_digits[i + 1] = carry;
     });
 
     new_digits
 }
 
-fn is_only_pow2(digits: Vec<u8>) -> bool {
+fn is_only_pow2(digits: [i8; DIGITS_LEN]) -> bool {
     digits.iter().all(|&m| { 
-         m == 1u8 || m == 2u8 || m == 4u8 || m == 8u8
+         m == 1 || m == 2|| m == 4|| m == 8 || m == -1 // unset
     })
 }
