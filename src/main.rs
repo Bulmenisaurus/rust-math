ce5bf400d6de7346036c6aa0ae1989ddcaf2a769
alexey <alexeybulmeni@gmail.com>
2022-11-05 14:55:31 -0700

    simplify code a tiny bit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-const DIGITS_LEN: usize = 30;
+const DIGITS_LEN: usize = 25;
 
 fn main() {
-    let mut digits = [-1; DIGITS_LEN];
+    let mut digits = [0; DIGITS_LEN];
     digits[0] = 1;
     let mut i: u64 = 0;
     println!("checking last {} digits", DIGITS_LEN);
@@ -9,10 +9,6 @@ fn main() {
     loop {
         if is_only_pow2(digits) {
             println!("found 2^{}", i);
-            // not 128
-            if i > 7 {
-                break;
-            }
         }
 
         if i % 10_000_000 == 0 {
@@ -25,42 +21,27 @@ fn main() {
 }
 
 // digits are stored back to front, least significant in the front
-fn mul_2_digits(digits: [i8; DIGITS_LEN]) -> [i8; DIGITS_LEN] {
-    let mut new_digits: [i8; DIGITS_LEN] = [-1; DIGITS_LEN];
-
-    // manually set the first digit to 0, as loop sets next digit with carry calculations
-    new_digits[0] = 0;
+fn mul_2_digits(digits: [u8; DIGITS_LEN]) -> [u8; DIGITS_LEN] {
+    let mut new_digits: [u8; DIGITS_LEN] = [0; DIGITS_LEN];
 
     digits.iter().enumerate().for_each(|(i, &item)| {
-        // if the digit is unset, stop (0 * 2 = 0)
-        if item == -1 {
-            return;
-        }
-
         // guaranteed to fit in, as item <= 9 and 2*item <= 18 < 127
         let new_digit_value = item * 2;
         let digit = new_digit_value % 10;
         let carry = (new_digit_value - digit) / 10;
 
-        if new_digits[i] == -1 {
-            new_digits[i] = digit;
-        } else {
-            new_digits[i] += digit;
-        }
+        new_digits[i] += digit;
 
-        let next_digit = carry;
         // if currently at the DIGITS_LENth digit, don't set next (as it is out of bounds)
         // otherwise, set the next digit to the carry otherwise
-        if i + 1 != DIGITS_LEN && carry != 0 {
-            new_digits[i + 1] = next_digit;
+        if i + 1 != DIGITS_LEN {
+            new_digits[i + 1] = carry;
         }
     });
 
     new_digits
 }
 
-fn is_only_pow2(digits: [i8; DIGITS_LEN]) -> bool {
-    digits.iter().all(|&m| {
-        m == 1 || m == 2 || m == 4 || m == 8 || m == -1 // unset
-    })
+fn is_only_pow2(digits: [u8; DIGITS_LEN]) -> bool {
+    digits.iter().all(|&m| m == 1 || m == 2 || m == 4 || m == 8)
 }
