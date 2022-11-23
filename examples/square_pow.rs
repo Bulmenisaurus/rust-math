7957a030b8d622bf4d0e1e401fa50fce1b2fec27
alexey <alexeybulmeni@gmail.com>
2022-11-23 15:05:16 -0800

    fix comments and other stuff

--- examples/square_pow.rs
+++ examples/square_pow.rs
@@ -12,18 +12,18 @@
 fn main() {
     let mut digits = [0; DIGITS_LEN];
     digits[0] = 1;
-    println!("{:?}", pow2_digits_slow(digits, 1364988100834))
+    println!("{:?}", exponentiate_by_squaring(digits, 1364988100834))
 }
 
-const DIGITS_LEN: usize = 10;
+const DIGITS_LEN: usize = 50;
 
 // square a number, using the naive multiplication algorithm
-fn pow_2_digits(digits: [u8; DIGITS_LEN]) -> [u8; DIGITS_LEN] {
+fn square_digits(digits: [u8; DIGITS_LEN]) -> [u8; DIGITS_LEN] {
     let mut new_digits: [u8; DIGITS_LEN] = [0; DIGITS_LEN];
 
     // distribute one number to the digits of the other, eg 34^2 = (34)(34) = 34(30) + 34(4)
     for i in 0..DIGITS_LEN {
-        let digits_multiplied = mul_digits(digits, digits[i]);
+        let digits_multiplied = mul_digits_by_digit(digits, digits[i]);
         // since multiplying by 10^n just means skipping n digits, we start adding the digits column by column at an offset
         for x in 0..DIGITS_LEN - i {
             let new_digit_value = new_digits[x + i] + digits_multiplied[x];
@@ -42,11 +42,11 @@ fn pow_2_digits(digits: [u8; DIGITS_LEN]) -> [u8; DIGITS_LEN] {
 }
 
 // digits are stored back to front, least significant in the front
-fn mul_digits(digits: [u8; DIGITS_LEN], n: u8) -> [u8; DIGITS_LEN] {
+fn mul_digits_by_digit(digits: [u8; DIGITS_LEN], n: u8) -> [u8; DIGITS_LEN] {
     let mut new_digits: [u8; DIGITS_LEN] = [0; DIGITS_LEN];
 
     digits.iter().enumerate().for_each(|(i, &item)| {
-        // guaranteed to fit in, as item <= 9 and 9*item <= 18 < 127
+        // guaranteed to fit in, as item <= 9, n <= 9, and <=9 * <=9 = <= 81 < u8::MAX = 127
         let new_digit_value = item * n;
         let digit = new_digit_value % 10;
         let carry = (new_digit_value - digit) / 10;
@@ -66,7 +66,7 @@ fn mul_digits(digits: [u8; DIGITS_LEN], n: u8) -> [u8; DIGITS_LEN] {
 // computes base^exponent mod modulus, in O(log(exponent)) multiplications
 // https://en.wikipedia.org/wiki/Exponentiation_by_squaring
 // recurse log_2(exponent) times
-fn pow2_digits_slow(base: [u8; DIGITS_LEN], exponent: u64) -> [u8; DIGITS_LEN] {
+fn exponentiate_by_squaring(base: [u8; DIGITS_LEN], exponent: u64) -> [u8; DIGITS_LEN] {
     if exponent == 0 {
         return base;
     }
@@ -74,15 +74,15 @@ fn pow2_digits_slow(base: [u8; DIGITS_LEN], exponent: u64) -> [u8; DIGITS_LEN] {
     if exponent % 2 == 0 {
         // 2^{2x} = (2^{x})^{2}
         // recursed_value = 2^{x}, so we need to return square(recursed_value)
-        let recursed_value = pow2_digits_slow(base, exponent / 2);
+        let recursed_value = exponentiate_by_squaring(base, exponent / 2);
 
-        return pow_2_digits(recursed_value);
+        return square_digits(recursed_value);
     } else {
         // 2^{2x + 1} = 2(2^{x})^{2}
         // we need to first square, then multiply by two
 
-        let recursed_value = pow2_digits_slow(base, (exponent - 1) / 2);
+        let recursed_value = exponentiate_by_squaring(base, (exponent - 1) / 2);
 
-        return mul_digits(pow_2_digits(recursed_value), 2);
+        return mul_digits_by_digit(square_digits(recursed_value), 2);
     }
 }
